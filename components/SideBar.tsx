f1026c6270e6073e96fb12e465f5b76e360b6ec0
daveclinton <clintondavid46@gmail.com>
2024-10-01 11:56:32 +0300

    feat: remove logut button

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -25,9 +25,6 @@ export const Sidebar = () => {
         <NavItem icon={CreditCard} label="My Cards" href="/" />
         <NavItem icon={List} label="Transactions" href="/" />
       </nav>
-      <Button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-        <LogOut className="mr-2 h-4 w-4 " /> Logout
-      </Button>
     </div>
   );
 };
