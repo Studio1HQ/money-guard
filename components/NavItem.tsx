a42cd2d8bb7f2efe826eccace974514b9aa1e291
daveclinton <clintondavid46@gmail.com>
2024-10-01 09:13:18 +0300

    feat: add navitem

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -6,8 +6,8 @@ type NavItemProps = {
 };
 
 export const NavItem: React.FC<NavItemProps> = ({ icon: Icon, label }) => (
-  <button className="flex flex-col items-center justify-center p-2 w-full hover:bg-gray-400 rounded">
-    <Icon size={24} />
-    <span className="text-xs mt-1">{label}</span>
+  <button className="flex flex-col lg:flex-row items-center justify-center lg:justify-start lg:gap-9 p-2 w-full rounded hover:text-orange-400">
+    <Icon size={24} className="transition-colors duration-300" />
+    <span className="text-xs mt-1 transition-colors duration-300">{label}</span>
   </button>
 );
